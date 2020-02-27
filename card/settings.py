0d432448cbabe55ac63956c7e9ae3c007dec6029
eunbi1997 <dmsql8811@likelion.org>
2020-02-27 21:56:49 +0900

    first

--- card/settings.py
+++ card/settings.py
@@ -131,6 +131,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 #static 파일들이 어디로 모일 것인지
 
 # : Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
